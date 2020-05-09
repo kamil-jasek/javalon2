889be65403390aeeb2b4939153562629f56922a8
Kamil Jasek <Kamil.Jasek@dbschenker.com>
2020-05-09 11:40:46 +0200

    converted to jpa (account)

--- src/main/java/pl/sda/shop/domain/Account.java
+++ src/main/java/pl/sda/shop/domain/Account.java
@@ -1,9 +1,12 @@
 package pl.sda.shop.domain;
 
-import pl.sda.shop.util.PreconditionUtil;
+import pl.sda.shop.util.annotation.JpaOnly;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 import static pl.sda.shop.util.PreconditionUtil.requireNonNull;
 
@@ -13,15 +16,32 @@ import static pl.sda.shop.util.PreconditionUtil.requireNonNull;
  * @author kamil.jasek@gmail.com
  * @since 2020-04-26
  */
+@Entity
+@Table(name = "accounts")
 public final class Account {
 
-    private final Customer customer;
-    private final List<Order> orders;
+    @Id
+    private UUID id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Customer customer;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Order> orders;
+
+    @JpaOnly
+    private Account() {
+    }
 
     public Account(Customer customer) {
         requireNonNull(customer);
+        this.id = UUID.randomUUID();
         this.customer = customer;
         this.orders = new ArrayList<>();
+    }
+
+    public UUID getId() {
+        return id;
     }
 
     public Customer getCustomer() {
@@ -33,4 +53,31 @@ public final class Account {
     }
 
     // methods for add order and remove order
+    public void addOrder(Order order) {
+        requireNonNull(order);
+        if (!orders.contains(order)) {
+            orders.add(order);
+        }
+    }
+
+    public Order removeOrder(int idx) {
+        if (idx >= 0 && idx < orders.size()) {
+            return orders.remove(idx);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return id.equals(account.id) &&
+                customer.equals(account.customer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, customer);
+    }
 }
---
+++ src/test/java/pl/sda/shop/domain/AccountTest.java
@@ -0,0 +1,48 @@
+package pl.sda.shop.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import java.math.BigDecimal;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * ... comment class...
+ *
+ * @author kamil.jasek@gmail.com
+ * @since 2020-05-09
+ */
+@SpringBootTest
+final class AccountTest {
+
+    @Autowired
+    private EntityManager em;
+
+    @Test
+    @Transactional
+    void testCreateAccount() {
+        var account = new Account(new Company("Test S.A.", new VatNumber("PL993993939")));
+        account.getCustomer()
+                .addAddress(new Address("Test1", "02-500", "Wawa", "PL"));
+        var order = new Order(asList(
+                new Item("test", new BigDecimal("10"), 1)
+        ));
+        order.applyDiscount(new CouponDiscount("ABC400", 0.3));
+        account.addOrder(order);
+
+        em.persist(account);
+        em.flush();
+        em.clear();
+
+        var readAccount = em.find(Account.class, account.getId());
+        assertEquals(readAccount, account);
+        assertEquals(new BigDecimal("7.00"), readAccount.getOrders()
+                .get(0).getTotalPrice());
+    }
+}
